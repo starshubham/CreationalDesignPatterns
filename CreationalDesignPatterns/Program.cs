da740eab99ceec49847a6c688c4b1ef3e26a0ca6
Shubham Seth <shubhamseth2312@outlook.com>
2026-09-18 23:18:20 +0530

    Add project files.

---
+++ CreationalDesignPatterns/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CreationalDesignPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
